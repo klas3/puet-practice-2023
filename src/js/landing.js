21862e57e2d9e4e665f3532c1a88588b8f9a4cc3
Ivan Shapovalov <sluvkann@gmail.com>
2023-05-16 08:01:27 +0300

    chore: add responsive design

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,33 +1,72 @@
-import Swiper from 'swiper';
-import 'swiper/css';
+/* eslint-disable */
 
-console.log('Landing...');
+import Swiper, { Navigation } from 'swiper';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 $(window).on('load', function () {
-  console.log('Load...');
-
   $('.statistics-blocks').slick({
     //autoplay: true,
     slidesToShow: 4,
     arrows: false,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 510,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 
-  const swiper = new Swiper('.swiper', {
+  new Swiper('.swiper', {
+    modules: [Navigation],
     loop: true,
-    // spaceBetween: 30,
+    spaceBetween: 30,
     slidesPerView: 4,
+    navigation: {
+      nextEl: '.advice-next',
+      prevEl: '.advice-prev',
+    },
+    breakpoints: {
+      100: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      510: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      750: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+    },
   });
 });
 
-$('#slick-next').on('click', function (evt) {
+$('.statistics-next').on('click', function () {
   $('.statistics-blocks').slick('slickNext');
 });
 
-$('#slick-prev').on('click', function (evt) {
+$('.statistics-prev').on('click', function () {
   $('.statistics-blocks').slick('slickPrev');
 });
-
-$(document).ready(function (evt) {
-  console.log('Ready...');
-});
-
